cb5d7ea0d9651ef6323157ad6aa69ec893b2704e
Anna <kupriienko16@gmail.com>
2024-05-23 19:28:41 +0100

    §

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,8 @@ import { GoTasklist } from "react-icons/go";
 import css from './HomePage.module.css'
 export default function HomePage() {
     return (
-        <div>
-            <h1 className={css.title} >Task manager welcome page <GoTasklist /> </h1>            
+        <div className={css.wrap}>
+            <h1 className={css.title} >Contacts book welcome page <GoTasklist /> </h1>            
         </div>
     )
 }
